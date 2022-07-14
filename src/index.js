81c0a2f196f98861efcaf7119e1227ffc12688d1
Uzair Ahmed Nasir <uzairahmednasir@outlook.com>
2022-07-14 23:37:48 +0500

    image add

--- src/index.js
+++ src/index.js
@@ -1,2 +1,9 @@
 import "./styles.css";
 
+fetch(
+  "https://api.unsplash.com/photos/random/?client_id=UKfhcy7SF1QT794tRwRsGHiJtgCXR0_X5FvTQVscXFE&orientation=landscape&query=nature"
+)
+  .then(response => response.json())
+  .then(data => {
+    document.body.style.backgroundImage = `url(${data.urls.full})`
+  });
